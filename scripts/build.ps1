62785f9d07fd742be1786782bbbecb83c66a9769
프리마 <fc1943s@pm.me>
2025-02-28 14:42:03 -0400

    tests

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -10,6 +10,24 @@ $ErrorActionPreference = "Stop"
 
 { pwsh ../../spiral/scripts/build.ps1 } | Invoke-Block
 
+if ($env:CI) {
+    Remove-Item ../../spiral/workspace/target/release/deps -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../spiral/workspace/target/release/build -Recurse -Force -ErrorAction Ignore
+}
+
 { pwsh ../../polyglot/scripts/build.ps1 } | Invoke-Block
 
+if ($env:CI) {
+    Remove-Item ../../polyglot/workspace/target/release/build -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../polyglot/workspace/target/release/deps -Recurse -Force -ErrorAction Ignore
+
+    Remove-Item ../../polyglot/target/spiral/spiral/target/debug/deps -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../polyglot/target/spiral/spiral/target/debug/incremental -Recurse -Force -ErrorAction Ignore
+}
+
 { pwsh ../../dice/scripts/build.ps1 } | Invoke-Block -OnError Continue
+
+if ($env:CI) {
+    Remove-Item ../../dice/target/release/deps -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../dice/target/release/build -Recurse -Force -ErrorAction Ignore
+}
