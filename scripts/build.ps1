dca2d94196786ff1a1a4a51801d54556cb39d9d0
프리마 <fc1943s@pm.me>
2025-03-05 03:24:44 -0400

    tests

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -18,11 +18,18 @@ if ($env:CI) {
 { pwsh ../../polyglot/scripts/build.ps1 } | Invoke-Block
 
 if ($env:CI) {
+    Remove-Item ../../polyglot/workspace/target/debug/build -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../polyglot/workspace/target/debug/deps -Recurse -Force -ErrorAction Ignore
     Remove-Item ../../polyglot/workspace/target/release/build -Recurse -Force -ErrorAction Ignore
     Remove-Item ../../polyglot/workspace/target/release/deps -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../polyglot/workspace/target/wasm32-unknown-unknown/debug/deps -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../polyglot/workspace/target/wasm32-unknown-unknown/release/deps -Recurse -Force -ErrorAction Ignore
 
     Remove-Item ../../polyglot/target/spiral/spiral/target/debug/deps -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../polyglot/target/spiral/spiral/target/debug/build -Recurse -Force -ErrorAction Ignore
     Remove-Item ../../polyglot/target/spiral/spiral/target/debug/incremental -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../polyglot/target/spiral/spiral/target/release/deps -Recurse -Force -ErrorAction Ignore
+    Remove-Item ../../polyglot/target/spiral/spiral/target/release/build -Recurse -Force -ErrorAction Ignore
 }
 
 { pwsh ../../dice/scripts/build.ps1 } | Invoke-Block -OnError Continue
