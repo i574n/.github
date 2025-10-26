ed37d8ec761ac46e989af1bd0e7a163169bce35a
프리마 <fc1943s@pm.me>
2025-10-26 14:05:36 -0400

    tests

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -6,8 +6,12 @@ Set-Location $ScriptDir
 $ErrorActionPreference = "Stop"
 
 
+$url = git ls-remote --get-url
+$owner = ($url -split '/' | Select-Object -Last 2 | Select-Object -First 1) -replace '\.git$', '' ?? $env:GITHUB_REPOSITORY_OWNER
+Write-Output "init.ps1 / url: $url / owner: $owner"
+
 Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
-git clone --recurse-submodules https://github.com/i574n/spiral.git # --branch gh-pages
+git clone --recurse-submodules https://github.com/$owner/spiral.git # --branch gh-pages
 Set-Location spiral
 git pull
 Set-Location $ScriptDir
@@ -18,7 +22,7 @@ pwsh ../../spiral/scripts/init.ps1
 . ../../polyglot/scripts/core.ps1
 
 Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
-git clone --recurse-submodules https://github.com/i574n/dice.git # --branch gh-pages
+git clone --recurse-submodules https://github.com/$owner/dice.git # --branch gh-pages
 Set-Location dice
 git pull
 Set-Location $ScriptDir
@@ -26,7 +30,7 @@ Set-Location $ScriptDir
 { pwsh ../../dice/scripts/init.ps1 } | Invoke-Block
 
 Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
-git clone --recurse-submodules https://github.com/i574n/alphabet.git # --branch gh-pages
+git clone --recurse-submodules https://github.com/$owner/alphabet.git # --branch gh-pages
 Set-Location alphabet
 git pull
 Set-Location $ScriptDir
